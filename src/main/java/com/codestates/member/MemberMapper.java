9d90bf496204f7f0c2999b59652893099a99a084
이재헌 <romeo92s@naver.com>
2022-10-25 15:59:16 +0900

    finish

--- src/main/java/com/codestates/member/MemberMapper.java
+++ src/main/java/com/codestates/member/MemberMapper.java
@@ -1,26 +1,35 @@
 package com.codestates.member;
 
 
-import org.springframework.stereotype.Component;
+import org.mapstruct.Mapper;
 
-@Component
-public class MemberMapper {
-    public Member memberPostDtoToMember(MemberPostDto memberPostDto) {
-        return new Member(0L,
-                memberPostDto.getEmail(),
-                memberPostDto.getName(),
-                memberPostDto.getPhone());
-    }
-    public Member memberPatchDtoToMember(MemberPatchDto memberPatchDto) {
-        return new Member(memberPatchDto.getMemberId(), null,
-                memberPatchDto.getName(),
-                memberPatchDto.getPhone());
-    }
+@Mapper(componentModel = "spring")
+public interface MemberMapper{
+    Member memberPostDtoToMember(MemberPostDto memberPostDto);
+    Member memberPatchDtoToMember(MemberPatchDto memberPatchDto);
 
-    public MemberResponseDto memberToMemberResponseDto(Member member) {
-        return new MemberResponseDto(member.getMemberId(),
-                member.getEmail(),
-                member.getName(),
-                member.getPhone());
-    }
+    MemberResponseDto memberToMemberResponseDto(Member member);
+
 }
+//
+//@Component
+//public class MemberMapper {
+//    public Member memberPostDtoToMember(MemberPostDto memberPostDto) {
+//        return new Member(0L,
+//                memberPostDto.getEmail(),
+//                memberPostDto.getName(),
+//                memberPostDto.getPhone());
+//    }
+//    public Member memberPatchDtoToMember(MemberPatchDto memberPatchDto) {
+//        return new Member(memberPatchDto.getMemberId(), null,
+//                memberPatchDto.getName(),
+//                memberPatchDto.getPhone());
+//    }
+//
+//    public MemberResponseDto memberToMemberResponseDto(Member member) {
+//        return new MemberResponseDto(member.getMemberId(),
+//                member.getEmail(),
+//                member.getName(),
+//                member.getPhone());
+//    }
+//}
